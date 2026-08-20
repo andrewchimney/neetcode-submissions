cd5aa38bf8e713a24866477cfa731991a76790ff
Andrew Weckwerth <93691894+andrewchimney@users.noreply.github.com>
2026-08-20 11:38:01 -0500

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        ret = 0
+        window = 0
+        ha = {}
+        left= 0
+        right=0
+        while(right<len(s)):
+            ha[s[right]] = ha.get(s[right],0)+1
+            window+=1
+            v=max(ha.values())
+            # print("highest repeated: ", v)
+            # print("window: ", window)
+            if(window>k+v):
+                # print("window too large")
+                ha[s[left]] = ha.get(s[left],0)-1
+                left+=1
+                window-=1
+            v=max(ha.values())
+            ret=max(ret, window)
+            right+=1
+        # print(ha)
+        return ret
